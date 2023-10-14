ed1f2e9ee95ff1bf25a1b7454e4c10235b3595c1
Sihle Ntuli <sihle.ntuli@glucode.com>
2023-10-14 10:19:26 +0200

    Update showAlert to update on the main thread

--- UdacityOnTheMap/UIViewControllers/UIViewExtensions/UIViewControllerExtension.swift
+++ UdacityOnTheMap/UIViewControllers/UIViewExtensions/UIViewControllerExtension.swift
@@ -10,13 +10,11 @@ import UIKit
 extension UIViewController {
     
     // MARK: Add Location action
-    
     @IBAction func addLocation(sender: UIBarButtonItem) {
         performSegue(withIdentifier: Constants.addLocationSegue, sender: sender)
     }
     
     // MARK: Enabled and disabled states for buttons
-    
     func buttonEnabled(_ enabled: Bool, button: UIButton) {
         if enabled {
             button.isEnabled = true
@@ -28,15 +26,15 @@ extension UIViewController {
     }
     
     // MARK: Show alerts
-    
     func showAlert(message: String, title: String) {
-        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        present(alertVC, animated: true)
+        DispatchQueue.main.async {
+            let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertVC, animated: true)
+        }
     }
     
     // MARK: Open links in Safari
-    
     func openLink(_ url: String) {
         guard let url = URL(string: url), UIApplication.shared.canOpenURL(url) else {
             showAlert(message: "Cannot open link.", title: "Invalid Link")

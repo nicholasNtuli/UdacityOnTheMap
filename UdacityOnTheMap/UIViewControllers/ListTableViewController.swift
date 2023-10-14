88af18c81074b6fbfd2b0b0f7484cb1ac2c48053
Sihle Ntuli <sihle.ntuli@glucode.com>
2023-10-14 10:19:00 +0200

    Remove default value

--- UdacityOnTheMap/UIViewControllers/ListTableViewController.swift
+++ UdacityOnTheMap/UIViewControllers/ListTableViewController.swift
@@ -57,10 +57,6 @@ class ListTableViewController: UITableViewController {
     }
 
     // MARK: Data source for the Table view
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return students.count
     }
